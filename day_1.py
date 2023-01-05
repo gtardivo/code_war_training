1cf634db6c7077e7af8f83a54385e78a5a7c5039
Guilherme Tardivo Pulzatto <guitardivo@hotmail.com>
2023-01-04 23:48:15 -0300

    day 1

---
+++ day_1.py
@@ -0,0 +1,15 @@
+#MY ANSWER
+def alphabet_position(text):
+    in_array = []
+    for c in text.lower():
+        if c in "abcdefghijklmnopqrstuvwxyz":
+            i = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].index(c)
+            in_array.append(i+1) 
+    return " ".join(str(x) for x in in_array)
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
+#print(alphabet_position("T"))
+
+#BEST ANSWER
+def alphabet_position(text):
+    return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
